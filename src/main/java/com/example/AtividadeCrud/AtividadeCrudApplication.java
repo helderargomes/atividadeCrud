9ba2bb10d11dadcbe602fb57e6d117c0e457acea
helderargomes <helderargomes@gmail.com>
2023-04-10 01:22:29 -0300

    Criação do projeto

---
+++ src/main/java/com/example/AtividadeCrud/AtividadeCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.AtividadeCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AtividadeCrud/AtividadeCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AtividadeCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
